b6a235cdc719ae4a155f7ef0ce9005e3d8c4dd62
Karol158 <159441394+Karol158@users.noreply.github.com>
2024-12-02 14:41:03 -0300

    Classificacao.java

--- Biblioteca/biblioteca/src/biblioteca/Classificacao.java
+++ Biblioteca/biblioteca/src/biblioteca/Classificacao.java
@@ -1,5 +1,18 @@
 package biblioteca;
 
 public class Classificacao {
+private String tipo;
 
+    public Classificacao(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+/* é uma agregação pois a classificação existe sem precisar dos livros*/
 }
