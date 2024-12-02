211f8ca8af084899c51cfa88ce095ab2ff702e6f
Karol158 <159441394+Karol158@users.noreply.github.com>
2024-12-02 14:41:48 -0300

    Dadosleitor.java

--- Biblioteca/biblioteca/src/biblioteca/Dadosleitor.java
+++ Biblioteca/biblioteca/src/biblioteca/Dadosleitor.java
@@ -1,5 +1,35 @@
 package biblioteca;
 
 public class Dadosleitor {
+ private String nome;
+    private String email;
+    private String telefone;
+ 
+   public Dadosleitor(String nome,String email,String telefone){
+     this.nome=nome;
+     this.email=email;
+     this.telefone=telefone;
+  }
+ public String getNome(){
+     return this.nome;
+ }
+ public void setNome(String nome){
+  this.nome=nome;
+ }
+ public String getEmail(){
+     return this.email;
+ }
+ public void setEmail(String email){
+     this.email=email;
+ }
+ public String getTelefone(){
+     return this.telefone;
+ }
+ public void setTelefone(String telefone){
+     this.telefone=telefone;
+ }
 
+ /* Essa classe é uma composição,pois para fazer o registro do livro e o leitor que está com o livro
+  precisa dos dados dele e do livro
+ */
 }
