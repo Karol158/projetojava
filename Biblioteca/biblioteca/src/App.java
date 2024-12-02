67d4d92264421e5ba7265eb66538e3a25fd6b708
Karol158 <159441394+Karol158@users.noreply.github.com>
2024-12-02 14:43:59 -0300

    App.java

--- Biblioteca/biblioteca/src/App.java
+++ Biblioteca/biblioteca/src/App.java
@@ -13,6 +13,8 @@ public class App {
          int ano_escrito = input.nextInt();
          System.out.println("Digite o genero literário:");
          String genero = input.nextLine();
+        System.out.println("Digite a classificação do livro:");
+         String tipo=input.nextLine()
          System.out.printf("Digite o ano que foi publicado:");
          int ano_publicado = input.nextInt();
          System.out.println("Digite a data que o livro foi pego:");
@@ -22,18 +24,29 @@ public class App {
          System.out.println("Digite o nome do leitor");
           String nome_leitor = input.nextLine();
          System.out.println("Digite a sua opiniao sobre o livro");
-          String opiniao = input.nextLine();
+          String feedback = input.nextLine();
+        System.out.println("Digite o nome do leitor:");
+           String nome = input.nextLine();
+        System.out.println("Digite o email do leitor:");
+        String email=input.nextLine();
+        System.out.println("Digite o telefone do leitor:");
+        String telefone=input.nextLine();
          
         System.out.println("Tipo de capa dp livro:" +capa);
         System.out.println("O nome do livro:"+nome_livro);
         System.out.println("O nome do autor:"+nome_autor);
         System.out.println("O ano que o livro foi escrito:"+ano_escrito);
         System.out.println("O genero literáro:"+genero);
+         System.out.println("O genero literáro:"+tipo);
         System.out.println("O ano que foi publicado:"+ano_publicado);
         System.out.println("A data que o livro foi pego:"+data_pegoulivro);
         System.out.println("A data que o livro foi devolvido:"+data_devolucao);
         System.out.println("O nome do leitor:"+nome_leitor);
-        System.out.println("Opiniao sobre o livro:"+opiniao);
+        System.out.println("Opiniao sobre o livro:"+feedback);
+         System.out.println("O genero literáro:"+nome);
+        System.out.println("O genero literáro:"+email);
+        System.out.println("O genero literáro:"+telefone);
+        
  
  
  
